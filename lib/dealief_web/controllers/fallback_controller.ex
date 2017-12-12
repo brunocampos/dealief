98da2c635cb096f3206a0ee293bad2a316f5bcc2
Bruno Campos <bruno@brunocampos.net>
2017-12-12 04:32:17 +0100

    Update fallback_controller to handle functions returning an error, :not_found,with custom message

--- lib/dealief_web/controllers/fallback_controller.ex
+++ lib/dealief_web/controllers/fallback_controller.ex
@@ -12,6 +12,12 @@ defmodule DealiefWeb.FallbackController do
     |> render(DealiefWeb.ChangesetView, "error.json", changeset: changeset)
   end
 
+  def call(conn, {:error, :not_found, message}) do
+    conn
+    |> put_status(:not_found)
+    |> json(%{error: message})
+  end
+
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
