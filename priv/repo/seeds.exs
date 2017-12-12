14d3ae726bab4f07067acb76be7f18b7d78fb688
Bruno Campos <bruno@brunocampos.net>
2017-12-12 04:29:44 +0100

    Add contract data to seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -29,3 +29,20 @@ vendors = [
 ]
 
 Enum.each(vendors, fn(vendor) -> Agreement.create_vendor(vendor) end)
+
+contracts = [
+  %{details: "5G - unlimited", ends_on: "10-11-2018", name: "O2 5G",
+    price: "29.99", starts_on: "12-04-2017", user_id: 1, vendor_id: 1},
+  %{details: "4G - unlimited", ends_on: "10-02-2018", name: "Vodafone 4G",
+    price: "25.99", starts_on: "23-02-2016", user_id: 2, vendor_id: 2},
+  %{details: "Verizon full plan", ends_on: "27-08-2018", name: "Verizon 5G",
+    price: "25.99", starts_on: "15-05-2016", user_id: 3, vendor_id: 3},
+  %{details: "Netflix 4K", ends_on: "27-04-2020", name: "Netflix 4k",
+    price: "19.50", starts_on: "03-06-2016", user_id: 1, vendor_id: 5},
+  %{details: "Netflix 4K", ends_on: "17-02-2019", name: "Netflix 4k",
+    price: "19.50", starts_on: "08-04-2017", user_id: 2, vendor_id: 5},
+  %{details: "Netflix 4K", ends_on: "25-10-2018", name: "Netflix 4k",
+    price: "19.50", starts_on: "01-01-2017", user_id: 3, vendor_id: 5}          
+]
+
+Enum.each(contracts, fn(contract) -> Agreement.create_contract(contract) end)
