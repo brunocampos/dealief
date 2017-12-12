4eeca4b81879c243cd4046e5ed26731938c84918
Bruno Campos <bruno@brunocampos.net>
2017-12-12 01:06:15 +0100

    Change create_users table migration to add email field as case insensitive and unique

--- priv/repo/migrations/20171211214654_create_users.exs
+++ priv/repo/migrations/20171211214654_create_users.exs
@@ -2,13 +2,16 @@ defmodule Dealief.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
+    # enable citext PostgreSQL extension for email field
+    execute "CREATE EXTENSION IF NOT EXISTS citext"
     create table(:users) do
       add :full_name, :string
-      add :email, :string
+      add :email, :citext
       add :password_hash, :string
 
       timestamps()
     end
 
+    create unique_index(:users, [:email])
   end
 end
